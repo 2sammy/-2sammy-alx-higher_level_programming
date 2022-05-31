6d37c0f38367245e43401e5194e901dcbfa42e7a
2sammy <samuelmanoah61@gmail.com>
2022-05-31 16:44:13 -0700

    else,if

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("{:d} = 0x{:x}".format(num, num))
-    
+for a in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+print("{:c}".format(a), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# 9-print_last_digit.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def print_last_digit(number):
-    last_digit = str(number)[-1]
-    last_digit = int(last_digit)
-    print(f"{last_digit:d}", end="")
-    return (last_digit)
-    
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
