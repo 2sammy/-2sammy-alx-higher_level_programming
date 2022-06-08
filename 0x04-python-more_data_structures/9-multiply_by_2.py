3beb3fb0ea1e52b2ec5468a9541b262c73dcdaf9
2sammy <samuelmanoah61@gmail.com>
2022-06-08 14:47:12 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# 8-simple_delete.py
-# Brennan D Baraban
+# 9-multiple_by_2.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return (a_dictionary)
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multipled by 2."""
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
