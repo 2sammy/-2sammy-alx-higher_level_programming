85aeeec69bab63c0c949a4c98af11160f267edd8
2sammy <samuelmanoah61@gmail.com>
2022-07-14 23:13:54 +0300

    inheritance

--- python_learning/inheritance.py
+++ python_learning/inheritance.py
@@ -1,16 +1,27 @@
-class Cat:
+class Pet :
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        def show(self):
+            print(f"am{self.name} and {self.age} old")
+
+class Cat(Pet):
+    #def __init__(self,name,age):
+        #self.name = name
+       # self.age = age
     def speak(self):
      print("meow")
     
-class Dog:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-        
+class Dog(Pet):
+    #def __init__(self,name,age):
+        #self.name = name
+        #self.age = age
     def speak(self):
         print("bark")    
                
-        
+#p = Pet("sam",19)
+#p.show()
+c = Cat("tracey",20)
+c.speak()
+d = Dog ("zalo",22)
+d.speak()
