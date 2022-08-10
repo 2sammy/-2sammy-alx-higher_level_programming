61798857b4a1bd0f37d60b2779e0cd691930bb9c
2sammy <samuelmanoah61@gmail.com>
2022-08-10 22:18:56 +0300

    user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creates the user user_0d_1 with all privileges.
+--Write a script that creates the MySQL server user user_0d_1
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
