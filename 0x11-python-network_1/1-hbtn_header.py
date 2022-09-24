d1a2d3bec536d5dbd6dbc20d581657bd82236f7b
2sammy <samuelmanoah61@gmail.com>
2022-09-24 15:20:12 -0700

    second

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+
