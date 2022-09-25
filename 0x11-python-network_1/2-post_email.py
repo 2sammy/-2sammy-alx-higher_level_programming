a89561f4add8f8a4511ff6f2a7ba378daf9ced74
2sammy <samuelmanoah61@gmail.com>
2022-09-25 11:40:48 -0700

    network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,6 @@
 import sys
 import urllib.request
 import urllib.parse
-
 if __name__ == "__main__":
     email = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
     req = urllib.request.Request(sys.argv[1], email)
