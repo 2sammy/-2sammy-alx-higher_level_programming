e037afa65fc72f253c1b9c5a4a3f1f415fab4d5e
2sammy <samuelmanoah61@gmail.com>
2022-09-25 11:59:32 -0700

    error status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,5 +15,4 @@ if __name__ == "__main__":
             html = resp.read()
             print("{}".format(html.decode('utf-8')))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    
+        print("Error code: {}".format(e.code))    
