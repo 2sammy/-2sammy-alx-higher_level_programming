372a5c4802f8365e0e3d2be722f79a6da9f71ba7
2sammy <samuelmanoah61@gmail.com>
2022-09-25 13:13:08 -0700

    status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+
 """
-    A script that takes in a URL and sends a request to the URL and displays the body of the r  esponse decoded in utf-8.urllib.error.HTTPError is managed and printed in the form
-     Error code: <HTTP status code>
- """
- import sys
- import urllib.request
- import urllib.parse
- if __name__ == "__main__":
-     req = urllib.request.Request(sys.argv[1])
-     try:
-         with urllib.request.urlopen(req) as resp:
-             html = resp.read()
-             print("{}".format(html.decode('utf-8')))
-     except urllib.error.HTTPError as e:                                                                 print("Error code: {}".format(e.code))
+A script that takes in a URL and sends a reque
+
+    and displays the body of the response deco
+
+    urllib.error.HTTPError is managed and prin
+
+    Error code: <HTTP status code>
+"""
+import sys
+import urllib.request
+import urllib.parse
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as re
+            html = resp.read()
+            print("{}".format(html.decode('utf
+    except urllib.error.HTTPError as e:
+
